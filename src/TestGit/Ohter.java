998939d5e88a054a0c782794fef74dbe7fe818b7
hx19941129 <874866024@qq.com>
2019-09-30 20:28:33 +0800

    modify something in java file

--- src/TestGit/Ohter.java
+++ src/TestGit/Ohter.java
@@ -1,5 +1,7 @@
 package TestGit;
 
 public class Ohter {
-
+	public static void main(String[] args) {
+		System.out.println("123456");
+	}
 }
--- src/TestGit/TestGitFuck.java
+++ src/TestGit/TestGitFuck.java
@@ -4,4 +4,7 @@ public class TestGitFuck {
 	public static void main(String[] args) {
 		System.out.println("hello github");
 	}
+	public void test() {
+		
+	}
 }
